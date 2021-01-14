9b0e92605b1ba586718eac6ea9ce9fc1327a69b1
=elirehema <=rocksandystone@gmail.com>
2021-01-14 18:06:47 +0300

    Dart pad file

---
+++ lib/dartpad/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/views/index.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Ospic Mobile',
+        theme: ThemeData(
+            primarySwatch: Colors.lightBlue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: Theme.of(context).textTheme.apply(
+                  fontFamily: 'Open Sans',
+                )),
+        home: HomeView());
+  }
+}
