6dce4678e20002a51f5e9913ae2054baa4f2b339
elirehema <29277048+elirehema@users.noreply.github.com>
2021-03-08 18:49:48 +0000

    Deploying to gh-pages from  @ ec963394fea74cdf57164f4db95c5135f06d604d 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,8 +22,8 @@ const RESOURCES = {
 "version.json": "d75b5ea4d59e2aafe556a5d9ddc482f3",
 "manifest.json": "bbf096d4b21132e3de3a45fec751771a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "14bd81ad3afc027e4a300a554484d80b",
-"/": "14bd81ad3afc027e4a300a554484d80b"
+"index.html": "a6d6bca23929af71d76136accd337fc0",
+"/": "a6d6bca23929af71d76136accd337fc0"
 };
 
 // The application shell files that are downloaded before a service worker can
