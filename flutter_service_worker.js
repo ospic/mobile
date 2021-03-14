f8bf79c0e6f63f590148bc760a8d391ffec90664
elirehema <29277048+elirehema@users.noreply.github.com>
2021-03-14 20:50:41 +0000

    Deploying to gh-pages from  @ ef5c62e703baa2c8719f8399c4f886a79f97d358 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "caa4d6e468ca30cc4f46f229628c4312",
+  "main.dart.js": "aed45f340f12a96b196836057d4efa09",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "assets/NOTICES": "8637cc178bdf4690de9ceb34b6eea294",
@@ -22,8 +22,8 @@ const RESOURCES = {
 "version.json": "d75b5ea4d59e2aafe556a5d9ddc482f3",
 "manifest.json": "bbf096d4b21132e3de3a45fec751771a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "ef0d0792681fdabc19f8afd1e860607a",
-"/": "ef0d0792681fdabc19f8afd1e860607a"
+"index.html": "e45c3dd6b162da0798789f8b564f6b6a",
+"/": "e45c3dd6b162da0798789f8b564f6b6a"
 };
 
 // The application shell files that are downloaded before a service worker can
