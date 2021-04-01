9e492ffc34bd644f9f9b5e707c8ed415870be0b6
elirehema <29277048+elirehema@users.noreply.github.com>
2021-04-01 20:57:29 +0000

    Deploying to gh-pages from  @ fcfbf19f3503c2a413929375137bb35136048aca 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,8 +7,8 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "bbf096d4b21132e3de3a45fec751771a",
-"index.html": "65521dde6662b8f570ba07a59bf2f805",
-"/": "65521dde6662b8f570ba07a59bf2f805",
+"index.html": "ccef4e7aeea248bd87200c3bbd42578c",
+"/": "ccef4e7aeea248bd87200c3bbd42578c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/progress_dialog/assets/double_ring_loading_io.gif": "e5b006904226dc824fdb6b8027f7d930",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "62fa0ce72ddf9a22ac36d88cf9b28bfb",
@@ -22,7 +22,7 @@ const RESOURCES = {
 "assets/fonts/Calibri-Light.ttf": "0d717c4bf80cbb556bf87777fef7d09e",
 "assets/AssetManifest.json": "2efe298caccfef0a0ba89edddd33e17c",
 "assets/FontManifest.json": "16129844116f3c3e11150aa941bf262c",
-"main.dart.js": "5f55f7d5502214a587247dee3a3266db",
+"main.dart.js": "becc0b23516a386a07a1a8ea5db9e8a3",
 "version.json": "d75b5ea4d59e2aafe556a5d9ddc482f3"
 };
 
