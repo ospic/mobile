a3a57d5d006040af7f35927c9180155249a044f3
Elirehema <rocksandystone@gmail.com>
2021-04-03 17:45:17 +0300

    View single transaction

--- lib/widgets/transaction_widget.dart
+++ lib/widgets/transaction_widget.dart
@@ -1,4 +1,6 @@
 import 'package:mobile/model/transaction.dart';
+import 'package:mobile/screens/index.dart';
+import 'package:mobile/model/index.dart';
 import 'package:mobile/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -16,9 +18,11 @@ class TransactionWidget extends StatefulWidget {
 }
 
 class _TransactionWidgetState extends State<TransactionWidget> {
+  
   @override
   Widget build(BuildContext context) {
     //var size = MediaQuery.of(context).size;
+    final Transaction transaction = widget.transaction;
     return  Container(
         decoration: BoxDecoration( //                    <-- BoxDecoration
           border: Border(bottom: BorderSide()),
@@ -29,10 +33,16 @@ class _TransactionWidgetState extends State<TransactionWidget> {
           leading: Icon(MdiIcons.account),
           trailing: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text(widget.transaction.amount.toString() +" "+ widget.transaction.currencyCode, style: TextStyle(fontWeight: FontWeight.bold),)],
+            children: [Text(transaction.amount.toString() +" "+ transaction.currencyCode, style: TextStyle(fontWeight: FontWeight.bold),)],
           ),
-          title:  Text((widget.transaction.medicalServiceName == null?  'Medicine:\t'+widget.transaction.medicineName.toString():'Service:\t'+widget.transaction.medicalServiceName.toString())),
-          subtitle:     Text('Date:'+widget.transaction.transactionDate,style: TextStyle(color: textPrimaryColor,  fontWeight: FontWeight.w200)),
+          title:  Text((transaction.medicalServiceName == null?  'Medicine:\t'+transaction.medicineName.toString():'Service:\t'+transaction.medicalServiceName.toString())),
+          subtitle:     Text('Date:'+transaction.transactionDate,style: TextStyle(color: textPrimaryColor,  fontWeight: FontWeight.w200)),
+          onTap:() => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      ViewTransaction(transaction.consultationId,transaction.id))),
+
         ));
   }
 }
