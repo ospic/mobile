e830ee8c13cbc44b2160ee771c0fcb6bba07d328
Elirehema <rocksandystone@gmail.com>
2021-06-16 16:50:14 +0300

    Add floating logout button

--- lib/screens/tabs/tab_home.dart
+++ lib/screens/tabs/tab_home.dart
@@ -1,14 +1,22 @@
-
+import 'package:mobile/data/post_api_service.dart';
+import 'package:mobile/model/consultation.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:mobile/utils/Constants.dart';
 import 'package:mobile/utils/colors.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:chopper/chopper.dart';
 
-class TabHome extends StatelessWidget {
+import '../view_consultation.dart';
+
+class TabHome extends StatefulWidget {
 
   TabHome();
-
+  _TabHomeTabState createState() => _TabHomeTabState();
+}
+  class _TabHomeTabState extends State<TabHome> {
   @override
   Widget build(BuildContext context) {
     return NestedScrollView(
@@ -46,54 +54,97 @@ class TabHome extends StatelessWidget {
           ),
         ];
       },
-      body: Container(
-        color: Constants.clr_light_blue,
-        child: new Container(
-            margin: EdgeInsets.symmetric(horizontal: 3.0),
-            decoration: new BoxDecoration(
-                color: Colors.white,
-                borderRadius: new BorderRadius.only(
-                  topLeft: const Radius.circular(20.0),
-                  topRight: const Radius.circular(20.0),
-                )),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(padding: EdgeInsets.only(left:15.0, bottom:5.0, top: 20.0),
-                    child: Text('CONSULTATIONS (20)',style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),),),
-
-        Flexible(
-            child: Container(
-              padding: EdgeInsets.all(5),
-              height: double.infinity,
-              width: double.infinity,
-              child:
-                  ListView.builder(
-                    itemCount:  11,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading:  ClipRRect(
-                          borderRadius: BorderRadius.circular(5.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: colorAccent,
-                            ),
-                            child: Icon(MdiIcons.drupal), height: 40.0, width: 40.0,),
-                        ),
-                        title: Text('Lorem ipsum dolor sit de amet ',style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black54),),
-                        subtitle: Text('So far everything is fine, behaves as we want'),
-                        trailing: Icon(MdiIcons.chevronRight),
-                      );
-                    },
-                  )))
-                ],
-
-            )
-        ),
-      ),
+      body: _buildBody(context),
 
     );
   }
+
+
+  FutureBuilder<Response<BuiltList<Consultation>>> _buildBody(
+      BuildContext context) {
+    return FutureBuilder<Response<BuiltList<Consultation>>>(
+      future: Provider.of<PostApiService>(context).getUserConsultations(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final BuiltList<Consultation> consultation = snapshot.data.body;
+         
+          return consultation.length > 0 ? _buildConsultationList(
+              context, consultation.reversed.toBuiltList()) : Center(
+            child: Text("No consultation found"),);
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildConsultationList(BuildContext context, BuiltList<Consultation> cs){
+    return  Container(
+      color: Constants.clr_light_blue,
+      child: new Container(
+          margin: EdgeInsets.symmetric(horizontal: 3.0),
+          decoration: new BoxDecoration(
+              color: Colors.white,
+              borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(20.0),
+                topRight: const Radius.circular(20.0),
+              )),
+          child:
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(padding: EdgeInsets.only(left:15.0, bottom:5.0, top: 20.0),
+                child: Text('CONSULTATIONS (${cs.length})',style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),),),
+
+              Flexible(
+                  child: Container(
+                      padding: EdgeInsets.all(5),
+                      height: double.infinity,
+                      width: double.infinity,
+                      child:
+                      ListView.separated(
+                        itemCount:  cs.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading:  ClipRRect(
+                              borderRadius: BorderRadius.circular(5.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: colorAccent,
+                                ),
+                                child: Icon(MdiIcons.drupal, color: cs[index].isActive ? Constants.clr_blue: null,), height: 40.0, width: 40.0,),
+                            ),
+                            title: Text(cs[index].fromDate,style: TextStyle( fontWeight: FontWeight.bold, color: Colors.black54),),
+                            subtitle: Text(cs[index].patientName),
+                            trailing: Icon(MdiIcons.chevronRight),
+                            onTap: (){
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) {
+                                return ScreenVieConsultation(cs[index].id);
+                              },
+                              ));
+                            },
+                          );
+                        },
+                        separatorBuilder: (context, index){
+                          return  Container(
+                              padding:  EdgeInsets.only(top: 1.0, bottom: 1.0),
+                              color: Constants.clr_light_blue, height: 1);
+                        },
+                      ),
+
+                  ))
+            ],
+
+          )
+      ),
+    );
+  }
+
+
 }
+
--- lib/utils/sharedpreference.dart
+++ lib/utils/sharedpreference.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile/utils/navigation_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-enum enumKey { IS_LOGGED_IN, BASE_64_EncodedAuthenticationKey }
+enum enumKey { IS_LOGGED_IN, BASE_64_EncodedAuthenticationKey, USER_NAME }
 
 class SharedPreference {
   SharedPreferences prefs;
