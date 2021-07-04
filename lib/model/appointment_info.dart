d7793769a889eb6cfb5bae673922a3ad449434ae
Elirehema <rocksandystone@gmail.com>
2021-07-04 19:08:41 +0300

    Update nullsafety in appoitment info

--- lib/model/appointment_info.dart
+++ lib/model/appointment_info.dart
@@ -3,13 +3,13 @@ import 'package:built_value/serializer.dart';
 
 part 'appointment_info.g.dart';
 abstract class AppointmentInfo implements Built<AppointmentInfo, AppointmentInfoBuilder>{
-  @nullable int get id;
-  @nullable int get patientId;
-  @nullable int get physicianId;
-  @nullable String get appointmentDate;
-  @nullable String get status;
-  @nullable String get job;
-  @nullable String get trigger;
+   int ? get id;
+   int ? get patientId;
+   int ? get physicianId;
+   String ? get appointmentDate;
+   String ? get status;
+   String ? get job;
+   String  ? get trigger;
   AppointmentInfo._();
   factory AppointmentInfo([updates(AppointmentInfoBuilder b)]) = _$AppointmentInfo;
   static Serializer<AppointmentInfo> get serializer => _$appointmentInfoSerializer;
