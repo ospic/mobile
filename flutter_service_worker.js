b8b35a20fc430f8e8f4efecad8553b14c7ee944d
elirehema <29277048+elirehema@users.noreply.github.com>
2021-07-04 21:48:17 +0000

    Deploying to gh-pages from @ ospic/mobile@5a300086b3c794e6108915f34f1e2270eb723fdf 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "2765281b412bd2ee27c1677e8f5f1ea2",
-"main.dart.js": "26d5ae9ce79ba46cc2503e2425fc64f4",
+"main.dart.js": "2c970e7f93ebb164175b3c552611fada",
 "index.html": "03397ead15ecfafda6887878c8858e1b",
 "/": "03397ead15ecfafda6887878c8858e1b",
 "manifest.json": "bbf096d4b21132e3de3a45fec751771a",
