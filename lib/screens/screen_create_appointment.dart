3704c65e42d37170246521d80fae453d95989b04
Elirehema <rocksandystone@gmail.com>
2021-07-05 23:53:20 +0300

    Create appoitment date picker

--- lib/screens/screen_create_appointment.dart
+++ lib/screens/screen_create_appointment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/utils/Constants.dart';
+import 'package:mobile/utils/colors.dart';
 
 class ScreenCreateAppointment extends StatelessWidget{
   @override
@@ -14,7 +15,43 @@ class ScreenCreateAppointment extends StatelessWidget{
         ),
         backgroundColor: Constants.clr_light_blue,
       ),
-      body: Center(child: Text('Create appointment'),),
+      body: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Appointment date',style: TextStyle(fontWeight: FontWeight.bold),),
+              SizedBox(height: 10.0,),
+              Container(
+                height: 100,
+                child: CupertinoDatePicker(
+                  mode: CupertinoDatePickerMode.date,
+                  initialDateTime: DateTime(1969, 1, 1),
+                  onDateTimeChanged: (DateTime newDateTime) {
+                    // Do something
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: ElevatedButton(
+            style: ButtonStyle(backgroundColor: MaterialStateProperty.all(colorPrimary)),
+            onPressed: () {
+            },
+            child: Text('Create appointment', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+        elevation: 0,
+      ),
     );
   }
 
