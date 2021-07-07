40feab625627ab0f9645d734222fcc4880d84d63
Elirehema <rocksandystone@gmail.com>
2021-07-07 19:34:47 +0300

    Implement deletion and scheduling of appointment

--- lib/screens/view_appointment.dart
+++ lib/screens/view_appointment.dart
@@ -4,33 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:mobile/data/post_api_service.dart';
 import 'package:mobile/model/appointment.dart';
 import 'package:mobile/utils/Constants.dart';
+import 'package:mobile/utils/colors.dart';
 import 'package:mobile/widgets/widget_something_happened.dart';
 import 'package:provider/provider.dart';
 
-class ScreenViewAppointment extends  StatelessWidget{
+class ScreenViewAppointment extends StatefulWidget {
   final int appointmentId;
   ScreenViewAppointment(this.appointmentId);
 
+  _ScreenViewAppointmentState createState() => _ScreenViewAppointmentState();
+}
+class _ScreenViewAppointmentState extends State<ScreenViewAppointment>{
+  var horizontalPadding = 30.0;
+  var verticalPadding = 15.0;
+  var radiusCircle = 30.0;
+  @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text('View appointment $appointmentId',style: TextStyle(color: Constants.clr_blue),),
-       elevation: 0,
-       iconTheme: IconThemeData(
-           color: Constants.clr_blue
-       ),
-       actions: [
-     IconButton(
-       icon: const Icon(Icons.edit),
-     color: Colors.black54,
-     onPressed: () {},
-   ),
-       ],
-       backgroundColor: Constants.clr_light_blue,
-     ),
-       resizeToAvoidBottomInset: false,
-       body: _buildBody(context, appointmentId));
+    return _buildBody(context, widget.appointmentId);
   }
+
 
   FutureBuilder<Response<Appointment>> _buildBody(BuildContext context, int id) {
     return FutureBuilder<Response<Appointment>>(
@@ -38,10 +30,11 @@ class ScreenViewAppointment extends  StatelessWidget{
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
           final Appointment? appointment = snapshot.data?.body;
-          return _updateAppointmentView(context, appointment);
-        } else if(snapshot.hasError){
+
+          return _updateAppointmentHeader(context, appointment);
+        } else if (snapshot.hasError) {
           return SomethingWrongHasHappened();
-        }  else {
+        } else {
           return Center(
             child: CircularProgressIndicator(),
           );
@@ -50,38 +43,131 @@ class ScreenViewAppointment extends  StatelessWidget{
     );
   }
 
-  Widget _updateAppointmentView(BuildContext context, Appointment? appointment){
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Card(
-          margin: EdgeInsets.all(5.0),
-          child: ListTile(
-            subtitle: Center(child: Text('Appointment date', style: TextStyle(fontSize: 16.0),)),
-            title: Center(child: Text(appointment!.appointment.appointmentDate!,  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold, color: Constants.clr_blue))),
+  Widget _updateAppointmentHeader(BuildContext context, Appointment? appointment){
+    return  Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 180.0,
+            collapsedHeight: 180.0,
+            primary: true,
+            floating: true,
+            pinned: false,
+            automaticallyImplyLeading: true,
+            backgroundColor: Constants.clr_blue,
+            foregroundColor: Colors.white,
+            title: Text('Appointment request', style: TextStyle(fontWeight: FontWeight.w200),),
+            elevation: 0.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title:  Text('${appointment!.appointment.appointmentDate}',style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 50.0), ),
+              stretchModes: <StretchMode>[
+                StretchMode.blurBackground
+              ],
+            ),
+
+            shape: ContinuousRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(0), bottomRight: Radius.circular(80))),
 
           ),
-        ),
-        SizedBox(height: 34.0,),
-        Center(child: Text('Physician'),),
-        Card(
-          child:Padding(
-            padding: EdgeInsets.all(10.0),
-            child:  ListTile(
-              leading: CircleAvatar(
-                radius: 35.0,
-                backgroundColor: appointment.staff.isActive! ?  Colors.red[700]: Colors.green[100],
-                child: appointment.staff.isActive!  ? Icon(Icons.ac_unit, color: Colors.white,): Text(
-                  appointment.staff.username!,
-                  style: TextStyle(fontWeight: FontWeight.bold, color:  appointment.staff.isActive!  ? Colors.white : Colors.green),
-                ),
-              ),
-              title:  Text(appointment.staff.fullName==null? appointment.staff.username! : appointment.staff.fullName!,style: TextStyle(fontWeight: FontWeight.bold), ),
-              subtitle:  Text('Email ${appointment.staff.email}' ),
-            ),
-          )
-        )
-      ],
+          SliverToBoxAdapter(
+            child: _updateAppointmentView(context, appointment),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        child:  Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Visibility(
+                visible: appointment.appointment.status == 'WAITING',
+                child: ElevatedButton(
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Constants.clr_light_blue),
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: verticalPadding, horizontal: horizontalPadding)),
+                  elevation: MaterialStateProperty.all(0.0),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.all( Radius.circular(radiusCircle))))),
+              onPressed: () async {
+                    _requestReschedulingOfAppointment(context, appointment.appointment.id);
+              },
+              child: Text('UN-SCHEDULE', style: TextStyle(color: Constants.clr_blue)),
+            )),
+            Visibility(
+                visible: appointment.appointment.status == 'WAITING',
+                child: ElevatedButton(
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Constants.clr_blue),
+                      padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: verticalPadding, horizontal: horizontalPadding)),
+                      elevation: MaterialStateProperty.all(0.0),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.all( Radius.circular(radiusCircle))))),
+                  onPressed: () async {},
+                  child: Text('EDIT', style: TextStyle(color: Colors.white)),
+                )),
+            Visibility(
+              visible: appointment.appointment.status == 'CANCELLED',
+              child: ElevatedButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Constants.clr_red),
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: verticalPadding, horizontal:  horizontalPadding)),
+                  elevation: MaterialStateProperty.all(0.0),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.all( Radius.circular(radiusCircle))))),
+              onPressed: () async {
+                _requestDeletionOfAppointment(context, appointment.appointment.id);
+              },
+              child: Text('DELETE', style: TextStyle(color: Colors.white)),
+            ),),
+
+          ],),
+        elevation: 0,
+      ),
     );
   }
+
+  Widget _updateAppointmentView(BuildContext context, Appointment? appointment) {
+
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 3.0),
+        decoration: new BoxDecoration(
+            color: Colors.white, borderRadius: new BorderRadius.only(
+          topLeft: const Radius.circular(-40.0),
+          topRight: const Radius.circular(20.0),)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                radius: 20.0,
+                backgroundColor: appointment!.staff.isActive! ? Constants.clr_blue : Colors.green[100],
+                child: appointment.staff.isActive! ? Icon(
+                  Icons.ac_unit, color: Colors.white,) : Text(appointment.staff.username!, style: TextStyle(fontWeight: FontWeight.bold, color: appointment.staff.isActive! ? Colors.white : Colors.green),
+                ),
+              ),
+              title: Text(appointment.staff.fullName == null ? appointment.staff.username! : appointment.staff.fullName!, style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24.0, color: Constants.clr_blue),),
+              subtitle: Text('Email ${appointment.staff.email}'),
+            ),
+          ],
+        ));
+  }
+}
+
+Future<void> _requestDeletionOfAppointment(BuildContext context, int? appointmentId) async {
+  Future<Response> response = Provider.of<PostApiService>(context, listen: false).deleteAppointmentByd(appointmentId!);
+  response.then((value) => {
+    Navigator.of(context).pop(),
+    if(value.statusCode == 200){
+      Navigator.of(context).pop()
+    }
+  });
+
+}
+
+
+Future<void> _requestReschedulingOfAppointment(BuildContext context, int? appointmentId) async {
+  Future<Response> response = Provider.of<PostApiService>(context, listen: false).unScheduleAppointmentById(appointmentId!);
+  response.then((value) => {
+    Navigator.of(context).pop(),
+    if(value.statusCode == 200){
+      print(value.statusCode),
+      Navigator.of(context).pop()
+    }
+  });
+
 }
