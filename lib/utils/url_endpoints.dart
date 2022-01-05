f9707a466b8a79f4f846d7b75d5b60a84bcbc434
elirehema <rocksandystone@gmail.com>
2022-01-05 22:39:06 +0300

    Added light and dark theme

--- lib/utils/url_endpoints.dart
+++ lib/utils/url_endpoints.dart
@@ -1,6 +1,5 @@
 class UrlEndpoints{
-  static const String RELEASE_BASE_URL ="http://192.168.147.167:8080/api/self";
-  static const String DEBUG_BASE_URL ="http://192.168.147.167:8080/api/self";
+  static const String RELEASE_BASE_URL ="http://192.168.10.167:8080/api/self";
   // ignore: non_constant_identifier_names
   static  String IMAGE_BASE_URL = RELEASE_BASE_URL.substring(0, RELEASE_BASE_URL.length - 5);
 
