29aa2659f2d0cc6500ad3854b6048cd71d71a6b0
elirehema <rocksandystone@gmail.com>
2022-03-06 01:08:16 +0300

    Update profile image url

--- lib/utils/url_endpoints.dart
+++ lib/utils/url_endpoints.dart
@@ -1,6 +1,6 @@
 class UrlEndpoints {
   static const String RELEASE_BASE_URL = "https://api.ospicx.com/api/self";
   // ignore: non_constant_identifier_names
-  static  String IMAGE_BASE_URL = RELEASE_BASE_URL.substring(0, RELEASE_BASE_URL.length - 5);
+  static  String IMAGE_BASE_URL = RELEASE_BASE_URL.substring(0, RELEASE_BASE_URL.length - 4);
 
 }
